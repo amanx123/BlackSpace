29e5fb83cbc1505236bce2c940cb79aea475df37
aman <amanagarwalx123@gmail.com>
2023-12-13 12:34:34 +0530

    maintenance

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='text-4xl font-bold h-screen flex justify-center items-center mx-auto flex-wrap'>
+    <div className='text-4xl font-bold h-screen flex justify-center items-center mx-auto flex-wrap px-8'>
       Under Maintenance, will launch soon !
     </div>
 
